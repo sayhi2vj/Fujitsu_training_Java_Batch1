3ade7c221a7b5b49b27479bb4081da58936e1fab
sayhi2vj <sayhi2vj@users.noreply.github.com>
2018-09-05 06:39:45 +0530

    Day 2

--- Day2/UppLowCase.java
+++ Day2/UppLowCase.java
@@ -1,42 +1,43 @@
-class UppLowCase{
+package workbook;
 
-public static void main(String[] args){
+public class UpLowerCase {
 
-String firstname = "vijay";
-String lastname = "kumar";
-System.out.print(firstname.toUpperCase());
-System.out.print(lastname.toLowerCase());
+	public static void main(String[] args) {
 
-if (firstname == "vijay"){
-System.out.print("valid");
-}else{
-System.out.print("invalid");
-}
+		String firstname = "vijay";
+		String lastname = "kumar";
+		System.out.print(firstname.toUpperCase());
+		System.out.print(lastname.toLowerCase());
 
-if(firstname.equals("vijay")){
-System.out.print("valid");
-}else{
-System.out.print("invalid");
-}
+		if (firstname == "vijay") {
+			System.out.print("valid");
+		} else {
+			System.out.print("invalid");
+		}
 
-if(firstname.equalsIgnoreCase("Vijay"))
-{
-System.out.print("valid");
-}else{
-System.out.print("invalid");
-}
+		if (firstname.equals("vijay")) {
+			System.out.print("valid");
+		} else {
+			System.out.print("invalid");
+		}
 
-if(firstname.startsWith("vij")){
-System.out.print("valid");
-}else{
-System.out.print("invalid");
-}
+		if (firstname.equalsIgnoreCase("Vijay")) {
+			System.out.print("valid");
+		} else {
+			System.out.print("invalid");
+		}
 
-if(firstname.contains("vijay")){
-System.out.print("valid");
-}else{
-System.out.print("invalid");
-}
+		if (firstname.startsWith("vij")) {
+			System.out.print("valid");
+		} else {
+			System.out.print("invalid");
+		}
 
-}
+		if (firstname.contains("vijay")) {
+			System.out.print("valid");
+		} else {
+			System.out.print("invalid");
+		}
+
+	}
 }
