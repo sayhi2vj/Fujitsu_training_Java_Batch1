8cb3829cbe0dd89dd51b3bfe224d0239a5c20121
sayhi2vj <sayhi2vj@users.noreply.github.com>
2018-09-05 06:37:01 +0530

    Day 2

---
+++ Day2/UppLowCase.java
@@ -0,0 +1,42 @@
+class UppLowCase{
+
+public static void main(String[] args){
+
+String firstname = "vijay";
+String lastname = "kumar";
+System.out.print(firstname.toUpperCase());
+System.out.print(lastname.toLowerCase());
+
+if (firstname == "vijay"){
+System.out.print("valid");
+}else{
+System.out.print("invalid");
+}
+
+if(firstname.equals("vijay")){
+System.out.print("valid");
+}else{
+System.out.print("invalid");
+}
+
+if(firstname.equalsIgnoreCase("Vijay"))
+{
+System.out.print("valid");
+}else{
+System.out.print("invalid");
+}
+
+if(firstname.startsWith("vij")){
+System.out.print("valid");
+}else{
+System.out.print("invalid");
+}
+
+if(firstname.contains("vijay")){
+System.out.print("valid");
+}else{
+System.out.print("invalid");
+}
+
+}
+}
